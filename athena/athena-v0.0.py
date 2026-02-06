75cb9224b7ee2f32395e879a89eb37a12a298adf
SeeSchloss <see@seos.fr>
2026-02-06 10:23:59 +0100

    Anciennes versions

---
+++ athena/athena-v0.0.py
@@ -0,0 +1,165 @@
+FIRMWARE_VERSION=1
+API_KEY="604588f010f068b496776a47b0c69404"
+PLACE_ID=14
+
+SENSOR_ID_BATTERY=57
+SENSOR_ID_SUPPLY=58
+SENSOR_ID_TEMPERATURE=56
+
+import microcontroller
+import time
+import alarm
+import board
+
+import busio
+import digitalio
+import analogio
+import sdcardio
+
+from ble import BLE
+
+import espcamera
+
+from ec200a import *
+from dcim import DCIM
+
+adc_battery =      analogio.AnalogIn(board.D8)
+adc_psu =          analogio.AnalogIn(board.D10)
+pin_power =        digitalio.DigitalInOut(board.D2)
+
+pin_EC_rx = board.D3
+pin_EC_tx = board.D4
+
+voltage_battery = adc_battery.value / 2**16 * 3.3 * 2
+voltage_supply = adc_psu.value / 2**16 * 3.3 * 2
+esp_temperature = microcontroller.cpu.temperature
+
+adc_battery.deinit()
+adc_psu.deinit()
+
+print(f"Battery voltage: {voltage_battery} V")
+print(f"Supply voltage: {voltage_supply} V")
+print(f"Temperature: {esp_temperature} °C")
+
+ble = BLE("ATHENA")
+data = ble.receive(timeout=2)
+if data:
+    print(f"Received BLE data: {data}")
+    voltage_psu = data[0]
+    voltage_battery = data[1]
+
+print(f"Battery voltage: {voltage_battery} V")
+print(f"Supply voltage: {voltage_supply} V")
+print(f"Temperature: {esp_temperature} °C")
+
+pin_led = digitalio.DigitalInOut(board.LED)
+pin_led.switch_to_output()
+pin_led.value = 0
+
+pin_power.switch_to_output()
+pin_power.value = 1
+
+cam = espcamera.Camera(
+    data_pins=board.CAM_DATA,
+    external_clock_pin=board.CAM_XCLK,
+    pixel_clock_pin=board.CAM_PCLK,
+    vsync_pin=board.CAM_VSYNC,
+    href_pin=board.CAM_HREF,
+    pixel_format=espcamera.PixelFormat.JPEG,
+    #frame_size=espcamera.FrameSize.VGA,
+    frame_size=espcamera.FrameSize.QSXGA,
+    i2c=busio.I2C(board.CAM_SCL, board.CAM_SDA),
+    external_clock_frequency=20_000_000,
+    framebuffer_count=2,
+    grab_mode=espcamera.GrabMode.LATEST)
+cam.hmirror = True
+cam.denoise = 2
+cam.quality = 4
+img = cam.take(2)
+
+if img is None:
+    # On essaie de reprendre une image avec une qualité inférieure, au cas où
+    cam.quality = 6
+    img = cam.take(2)
+
+if img is None:
+    # On essaie de reprendre une image avec une qualité inférieure, au cas où
+    cam.quality = 10
+    img = cam.take(2)
+
+if img is None:
+    # On essaie de reprendre une image avec une qualité inférieure, au cas où
+    cam.quality = 20
+    img = cam.take(2)
+
+if img is not None:
+    print(f"Picture size: {len(img)}")
+
+    pin_led.deinit()
+    try:
+        sd = sdcardio.SDCard(board.SPI(), board.SDCS)
+        dcim = DCIM(sd)
+        (result, filename) = dcim.store(img, ".JPG")
+        if result:
+            print(f"Image saved as '{filename}'")
+        else:
+            print(f"Image could not be saved, for some reason: {filename}")
+    except Exception as e:
+        print(f"Probably no SD card present: {e}")
+    finally:
+        sd.deinit()
+        board.SPI().deinit()
+
+    pin_led = digitalio.DigitalInOut(board.LED)
+    pin_led.switch_to_output()
+    pin_led.value = 0
+else:
+    print(f"Cannot take picture? {img}")
+    pin_led.value = 1
+
+pin_sleep_signal = digitalio.DigitalInOut(board.D9)
+pin_sleep_signal.switch_to_output()
+pin_sleep_signal.value = 0
+
+uart = busio.UART(rx=pin_EC_rx, tx=pin_EC_tx, baudrate=115200)
+http = HTTP_EC200A(uart, [
+    ("X-Api-Key", API_KEY),
+    ("X-Board-Id", microcontroller.cpu.uid.hex()),
+    ("User-Agent", f"Athena/{FIRMWARE_VERSION} (CircuitPython, EC200A-EU)"),
+])
+
+if http.init_modem(baudrate=230400, timeout=25000):
+    time.sleep(2)
+    network_time = http.network_time()
+
+    print(f"Network time: {network_time}")
+
+    http.send_http_post_json("http://veranda.seos.fr/data/sensor", f'{{ "{SENSOR_ID_BATTERY}": {voltage_battery}, "{SENSOR_ID_SUPPLY}": {voltage_supply}, "{SENSOR_ID_TEMPERATURE}": {esp_temperature} }}')
+    time.sleep(1)
+
+    if img is not None:
+        try:
+            result = http.send_file(f"http://veranda.seos.fr/data/place/{PLACE_ID}/photo", img)
+            print(f"Picture sent {result}")
+        except Exception as e:
+            print(f"Exception while sending picture? {e}")
+            pin_led.value = 1
+
+    http.set_uart_speed(115200)
+    time.sleep(10)
+else:
+    print(f"Couldn't init modem")
+
+print(f"Time to sleep, waiting for 10 seconds to allow going into REPL")
+pin_power.value = 0
+
+time.sleep(10)
+pin_led.value = 0
+
+print(f"Really sleeping now")
+
+pin_sleep_signal.value = 1
+time.sleep(1)
+
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 300)
+alarm.exit_and_deep_sleep_until_alarms(time_alarm)
---
+++ athena/athena-v0.3.py
@@ -0,0 +1,171 @@
+FIRMWARE_VERSION=1
+ATHENA_URL="http://athena.seos.fr"
+
+import microcontroller
+import time
+import alarm
+import board
+
+import busio
+import digitalio
+import analogio
+import sdcardio
+
+import json
+
+from ble import BLE
+
+from ov5640 import Cam
+
+from ec200a import *
+from dcim import DCIM
+
+adc_battery =      analogio.AnalogIn(board.D8)
+adc_psu =          analogio.AnalogIn(board.D10)
+pin_power =        digitalio.DigitalInOut(board.D2)
+
+pin_EC_rx = board.D3
+pin_EC_tx = board.D4
+
+time_start = time.monotonic()
+
+voltage_battery = adc_battery.value / 2**16 * 3.3 * 2
+voltage_supply = adc_psu.value / 2**16 * 3.3 * 2
+esp_temperature = microcontroller.cpu.temperature
+
+adc_battery.deinit()
+adc_psu.deinit()
+
+print(f"Battery voltage: {voltage_battery} V")
+print(f"Supply voltage: {voltage_supply} V")
+print(f"Temperature: {esp_temperature} °C")
+
+ble = BLE("ATHENA")
+data = ble.receive(timeout=2)
+if data:
+    print(f"Received BLE data: {data}")
+    voltage_psu = data[0]
+    voltage_battery = data[1]
+
+print(f"Battery voltage: {voltage_battery} V")
+print(f"Supply voltage: {voltage_supply} V")
+print(f"Temperature: {esp_temperature} °C")
+
+pin_led = digitalio.DigitalInOut(board.LED)
+pin_led.switch_to_output()
+pin_led.value = 0
+
+pin_power.switch_to_output()
+pin_power.value = 1
+
+img = None
+img_quality = 0
+
+try:
+    camera = Cam()
+    img = camera.take()
+    img_quality = camera.cam.quality
+
+except Exception as e:
+    print(f"Cannot take picture: {e}")
+    img = None
+
+if img is not None:
+    print(f"Picture size: {len(img)}")
+
+    sd = None
+    pin_led.deinit()
+    try:
+        sd = sdcardio.SDCard(board.SPI(), board.SDCS)
+        dcim = DCIM(sd)
+        (result, filename) = dcim.store(img, ".JPG")
+        if result:
+            print(f"Image saved as '{filename}'")
+        else:
+            print(f"Image could not be saved, for some reason: {filename}")
+    except Exception as e:
+        print(f"Probably no SD card present: {e}")
+    finally:
+        if sd is not None:
+            sd.deinit()
+        board.SPI().deinit()
+
+    pin_led = digitalio.DigitalInOut(board.LED)
+    pin_led.switch_to_output()
+    pin_led.value = 0
+else:
+    print(f"Cannot take picture? {img}")
+    pin_led.value = 1
+
+pin_sleep_signal = digitalio.DigitalInOut(board.D9)
+pin_sleep_signal.switch_to_output()
+pin_sleep_signal.value = 0
+
+try:
+    uart = busio.UART(rx=pin_EC_rx, tx=pin_EC_tx, baudrate=115200)
+    http = HTTP_EC200A(uart, [
+        ("X-Board-Id", microcontroller.cpu.uid.hex()),
+        ("User-Agent", f"Athena/{FIRMWARE_VERSION} (CircuitPython, EC200A-EU)"),
+    ])
+
+    if http.init_modem(baudrate=921600, timeout=25000):
+        time.sleep(2)
+        network_time = http.network_time()
+
+        print(f"Network time: {network_time}")
+
+        img_size = 0
+        if img is not None:
+            img_size = len(img)
+
+        http.send_http_post_json(f"{ATHENA_URL}/data/sensor", json.dumps({
+            "battery": {"value": voltage_battery, "type": "voltage"},
+            "supply": {"value": voltage_supply, "type": "voltage"},
+            "temperature": {"value": esp_temperature, "type": "temperature"},
+            "image_size": {"value": img_size, "name": "Image size"},
+            "image_quality": {"value": img_quality, "name": "Image quality"},
+        }))
+
+        time.sleep(1)
+
+        time_transfer_start = time.monotonic()
+
+        if img is not None:
+            result = http.send_file(f"{ATHENA_URL}/data/photo", img)
+            print(f"Picture sent {result}")
+
+
+        time_stop = time.monotonic()
+
+        http.set_uart_speed(115200)
+
+        http.send_http_post_json(f"{ATHENA_URL}/data/sensor", json.dumps({
+            "duration_total": {"value": time_stop - time_start, "type": "duration", "name": "Total duration"},
+            "duration_transfer": {"value": time_stop - time_transfer_start, "type": "duration", "name": "Transfer duration"},
+        }))
+
+        time.sleep(1)
+
+        http.modem_shutdown()
+        time.sleep(10)
+    else:
+        print(f"Couldn't init modem")
+        http.set_uart_speed(115200)
+        http.modem_shutdown()
+except Exception as e:
+    print(f"Exception while sending picture? {e}")
+    pin_led.value = 1
+
+print(f"Time to sleep, waiting for 10 seconds to allow going into REPL")
+pin_power.value = 0
+
+time.sleep(10)
+pin_led.value = 0
+
+print(f"Really sleeping now")
+
+pin_sleep_signal.value = 1
+time.sleep(1)
+
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 300)
+alarm.exit_and_deep_sleep_until_alarms(time_alarm)
